27dbab655f59a8fd5428715d24ed72661c28ccda
Akash Doppalapudi <akashdoppalapudi2001@gmail.com>
2021-11-20 00:42:44 +0530

    Added new components

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
-<h1>Hello World</h1>
+<script>
+    import Timer from "./Timer.svelte";
+    import HowTo from "./HowTo.svelte";
+</script>
+
+<h1>Hand Washing App</h1>
+<Timer />
+<HowTo />
+
+<h3>
+    <a href="https://www.who.int/teams/integrated-health-services/infection-prevention-control" target="_blank">Picture Source</a>
+</h3>
+<h3><a href="https://freesound.org/people/metrostock99/sounds/345086/" target="_blank">Sound Source</a></h3>
+
+<style>
+    h1{
+        text-align: center;
+    }
+</style>
---
+++ src/HowTo.svelte
@@ -0,0 +1,9 @@
+<div bp="grid">
+    <img bp="offset-5@md 4@md 12@sm" src="handwashing.gif" alt="handwashing technique" />
+</div>
+
+<style>
+    img{
+        width: 100%;
+    }
+</style>
